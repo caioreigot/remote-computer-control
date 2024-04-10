e6710648c45423bd3b5780134f989864981011c1
frsouzaa <felipe.souza1506@outlook.com>
2024-04-09 23:17:18 -0300

    fix: ajustando link para o logo

--- src/views/app/get/get.py
+++ src/views/app/get/get.py
@@ -1,7 +1,7 @@
 from typing import Tuple, Dict
 import socket
-import base64
 from pathlib import Path
+from ....utils.logo import logo
 
 
 class Get():
@@ -13,8 +13,6 @@ class Get():
         s.connect(("8.8.8.8", 80))
         with open(f"{path.parent}/page.html", "r", encoding="utf-8") as f:
             html = f.read()
-        with open(f"{path.parent.parent.parent.parent}/assets/logo.png", "rb") as f:
-            icon = f.read()
         html = html.replace("##URL##", f"{s.getsockname()[0]}:5000")
-        html = html.replace("##ICON##", base64.b64encode(icon).decode())
+        html = html.replace("##ICON##", logo.decode())
         return html
