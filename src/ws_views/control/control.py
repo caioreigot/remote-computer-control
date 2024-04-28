3c92fc6b4d2ace433770ae7bad33a33280bfd984
caioreigot <caiocrrodrigues2004@gmail.com>
2024-04-27 23:38:13 -0300

    feature: aprimora a precisão de ponteiro no movimento do mouse & deixa o touchpad em tela cheia ao mover o mouse

--- src/ws_views/control/control.py
+++ src/ws_views/control/control.py
@@ -1,13 +1,29 @@
 from typing import Tuple, Dict, Any
+from math import copysign
 import mouse
 import pyautogui
 import json
 import keyboard
 import platform
 
-
 class Control():
-    
+    def accelerate(self, value, multiplier, max_mov_value):
+        min_input, max_input = 0, 10 
+        min_output, max_output = 0, 5
+        exponent = 2
+
+        sign = copysign(1, value)
+
+        input_range = max_input - min_input
+        output_range = max_output - min_output
+
+        normalized_value = (value - min_input) / input_range
+        accelerated_value = normalized_value ** exponent
+        mapped_value = min_output + (accelerated_value * output_range)
+        mapped_value = min(mapped_value * multiplier, max_mov_value)
+
+        mapped_value *= sign
+        return mapped_value
 
     def handle_request(self, input: Any) -> Tuple[Dict[str, any], int]:
         input = json.loads(input)
@@ -15,7 +31,11 @@ class Control():
         data: Dict[str, any] = input.get("data")
         if oper == "move":
             try:
-                mouse.move(data.get("x") * 4, data.get("y") * 4, absolute=False)
+                x_raw_mov = data.get("x")
+                y_raw_mov = data.get("y")
+                final_x_mov = self.accelerate(x_raw_mov, 5, 120)
+                final_y_mov = self.accelerate(y_raw_mov, 5, 120)
+                mouse.move(final_x_mov, final_y_mov, absolute=False)
                 return {"status": 200, "msg": "Sucesso"}
             except Exception as e:
                 return {"status": 500, "msg": "Erro"}
